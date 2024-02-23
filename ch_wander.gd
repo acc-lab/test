8645c43cc3186742ee6dc8a89705a721a5e8514d
acc-lab <63785690+acc-lab@users.noreply.github.com>
2024-02-23 11:55:45 +0800

    our commitment these fourty nights and days
    tsao xi xue

---
+++ ch_wander.gd
@@ -0,0 +1,65 @@
+extends KinematicBody2D
+
+onready var animator = $animator
+onready var sprite = $AnimatedSprite
+var state = "walk"
+
+var machine_eps = 1.0
+
+func _ready():
+	self.position.y = 400
+	animator.current_animation = "idle"
+	
+	# process the machine epsilon
+	while machine_eps + 1 > 1:
+		machine_eps /= 2
+	machine_eps *= 2
+
+"""
+While I used the machine epsilon, it's worthy to note that the "dur" or "tick" in the _process function
+can still accumulate deviation. On the other hand, since the delta time fluctuates, the game logic
+(which entirely depends on when AnimationPlayer play animations) is not entirely deterministic. However,
+even though animations of AnimationPlayer and the _process mainloop are independent, we can still expect
+the _process function delay is globally the same among characters (skeletons) (*right?).
+"""
+
+func strict_greater(a, b):
+	return not (a <= b+machine_eps)
+	
+var tick = 0
+
+var bound_x = 600
+var y = 400
+var last_tick = 0
+
+func _process(delta):
+	tick += delta
+	
+	var dur = tick - last_tick
+	
+	if(!animator.current_animation): animator.current_animation = "idle"
+	
+	if(state == "walk" and strict_greater(dur,1.08)):
+		state = "idle"
+		dur -= 1.08
+	elif(state == "idle"):
+		state = "walk"
+	
+	if(state != animator.current_animation):
+		animator.current_animation = state
+		last_tick = tick
+		
+	if(self.position.x > 920):
+		self.position.x -= 940
+	
+	
+func _move(steps):
+	self.position.x += steps
+	 
+
+func _on_ax_skeleton_mouse_entered():
+	state = "attack"
+
+
+func _on_ax_skeleton_mouse_exited():
+	state = "walk"
