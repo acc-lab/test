4822fcbf7a6e0ea611b7334a3b22dbc26355033c
acc-lab <63785690+acc-lab@users.noreply.github.com>
2024-03-12 23:39:15 +0800

    cu(sto)mize / instantiating / team

--- ch_wander.gd
+++ ch_wander.gd
@@ -3,8 +3,12 @@ extends KinematicBody2D
 onready var animator = $animator
 onready var sprite = $AnimatedSprite
 
-var machine_eps = 1.0 # initial value
+var uuid
+var team = 1
+var observe_target_x
 var state = "idle"
+
+var machine_eps = 1.0 # initial value
 
 func _ready():
 	self.position.y = 400
@@ -25,40 +29,66 @@ the _process function delay is globally the same among characters (skeletons) (*
 
 func gneq(a, b):
 	return not (a <= b+machine_eps)
+
+func geq(a, b):
+	return (a >= b - machine_eps)
 	
 var tick = 0
-
-var bound_x = 600
-var y = 400
 var last_tick = 0
+var last_state
+
+func exceed(a, b, dir):
+	if dir > 0: return gneq(a, b)
+	else: return gneq(b, a)
 
 func cst_movement(dur):
-	if(state == "walk" and gneq(dur,0.42)):
+	if(state == "walk" and geq(dur,0.42)):
 		state = "idle"
-		dur -= 0.42
 	elif(state == "idle"):
-		state = "walk"
-	
+		if exceed(self.position.x + 450*getDir(), observe_target_x, getDir()):
+			state = "attack"
+		else:
+			state = "walk"
+	elif(state == "attack"):
+		if geq(dur, 0.33):
+			#print("shoot!")
+			state = "after_attack"
+	elif(state == "after_attack"):
+		if geq(dur, 1.17):
+			state = "idle"
+			#print("super idle")
+			
+	#print(uuid, " ", state)
 
 func _process(delta):
 	tick += delta
 	
 	var dur = tick - last_tick
 	
+	self.scale.x = 0.5*getDir()
 	if(!animator.current_animation): animator.current_animation = "idle"
 	
 	cst_movement(dur)
 	
-	if(state != animator.current_animation):
-		animator.current_animation = state
+	if(state != last_state):
+		var anim = {"walk":"walk", "attack":"attack", "idle":"idle", "after_attack":"idle"}[state]
+		animator.current_animation = anim
 		last_tick = tick
+		last_state = state
 		
 	#if(self.position.x > 920):
 	#	self.position.x -= 940
-	
+
+func getDir():
+	var direction
+	if team==1:
+		direction = 1
+	else:
+		direction = -1
+	return direction
 	
 func _move(steps):
-	self.position.x += steps
+	self.position.x += steps * getDir()
 	 
 
 func _on_ax_skeleton_mouse_entered():
--- test sprites.gd
+++ test sprites.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var scene = {}
-var scenes = ["archer", "axy", "healer", "police", "tank"]
+var scenes = ["archer2", "axy", "healer", "police", "tank"]
 
 func _ready():
 	for name in scenes:
@@ -10,11 +10,12 @@ func _ready():
 var rng = RandomNumberGenerator.new()
 
 var ticks = 0
+var UUID = 0
 
-func _process(delta):
-	ticks += delta
+func leftcpr(a, b):
+	return a.position.x < b.position.y
 	
-	"""
+func spawn_random():
 	if ticks > 1:
 		ticks -= 1
 		var RI = rng.randi_range(1, 100)
@@ -24,7 +25,44 @@ func _process(delta):
 			var sc = scene[scenes[rng.randi_range(0, 4)]]
 			
 			var instance = sc.instance()
-			instance.position.x = -20
+			instance.set("uuid", UUID)
+			if rng.randi_range(1, 100) >= 50:
+				instance.position.x = -20
+				instance.set("team", 1)
+			else:
+				instance.position.x = 920
+				instance.set("team", 2)
+			instance.set("observe_target_x", null)
+			UUID += 1
 			add_child(instance)
-	"""
+
+func _process(delta):
+	ticks += delta
+	
+	spawn_random()
+	
+	var children = get_children()
+	children.sort_custom(self, "leftcpr")
+	
+	var target = 20
+	var ch
+	for i in range(len(children)):
+		ch = children[i]
+		
+		if(ch.team == 1):
+			target = ch.position.x
+		else:
+			ch.observe_target_x = target
+			
+	target = 880
+	for i in range(len(children)-1, -1, -1):
+		ch = children[i]
+		
+		if(ch.team == 2):
+			target = ch.position.x
+		else:
+			ch.observe_target_x = target
+	
+	
+		
 			
