5abfececa53e1bdf70f8cd212014e1bf53db8a29
acc-lab <cabbagelab2023@gmail.com>
2025-04-01 11:21:24 +0800

    Fix font display and SFX bug

--- assets/sound_manager.gd
+++ assets/sound_manager.gd
@@ -3,26 +3,56 @@ extends Node
 var sounds = {}  # Dictionary to store preloaded sounds
 
 func _ready():
-	set_global_volume(0.03)
-	preload_sounds("res://assets/SFX/")  # Load all sounds from this folder
+	set_global_volume(0.1)
+	preload_sounds()  #; Load all sounds from this folder
 	
 func set_global_volume(volume: float):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear2db(volume))
 
-func preload_sounds(path: String):
-	var dir = Directory.new()
-	if dir.open(path) == OK:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		
-		while file_name != "":
-			if file_name.ends_with(".wav") or file_name.ends_with(".ogg") or file_name.ends_with(".mp3"):
-				var sound_path = path + file_name
-				sounds[file_name.get_basename()] = load(sound_path)
-				print("Loaded sound:", file_name)
-			file_name = dir.get_next()
-	else:
-		print("Error: Could not open directory:", path)
+func preload_sounds():
+	var sound_ext = ["wav"]
+	var files = [
+		"Alarm.wav",
+		"Bow.wav",
+		"Buy.wav",
+		"Chamber Coin",
+		"Chamber Coin 2.wav",
+		"Chicken.wav",
+		"Chicken 2.wav",
+		"Chicken 3.wav",
+		"Click.wav",
+		"Coin 2.wav",
+		"Coin 3.wav",
+		"Dash.wav",
+		"Dash 2.wav",
+		"Dooz.wav",
+		"Flesh Hit.wav",
+		"Gameover 3.wav",
+		"Jab.wav",
+		"Ka Ching.wav",
+		"Laser.wav",
+		"Level Complete.wav",
+		"Ping.wav",
+		"Reveal.wav",
+		"Revive.wav",
+		"RIP sound.wav",
+		"Select Good.wav",
+		"Splat.wav",
+		"Split Hit.wav",
+		"Dooz.wav",
+		"Flesh Hit.wav",
+		"Squeak.wav",
+		"Swing.wav",
+		"Swing 2.wav",
+		"Sword Slash.wav",
+		"Tear.wav",
+		"Unlock.wav",
+	]
+	
+	for file in files:
+		if sound_ext.has(file.get_extension()):
+			sounds[file.get_basename()] = ResourceLoader.load("res://assets/SFX/{name}".format({"name":file}))
+			print("Loaded sound: ", file)
 		
 func play_sound_random(sound_names: Array):
 	var random_sound_name = sound_names[randi() % sound_names.size()]
